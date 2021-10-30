81b6b9617a4112bed0e7b2f70efe14d7615a9a07
Sergey <gecktor87@gmail.com>
2021-10-30 19:07:31 +0300

    подключен Stetho инспектор

--- app/src/main/java/com/example/githubapp/App.kt
+++ app/src/main/java/com/example/githubapp/App.kt
@@ -3,10 +3,13 @@ package com.example.githubapp
 import android.app.Application
 import com.example.githubapp.data.datasource.GitHubApi
 import com.example.githubapp.data.domain.EventBus
+import com.facebook.stetho.Stetho
+import com.facebook.stetho.okhttp3.StethoInterceptor
 import com.github.terrakok.cicerone.Cicerone
 import com.github.terrakok.cicerone.Router
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -24,6 +27,16 @@ class App:Application() {
     val router get() = cicerone.router
 
     val eventBus = EventBus
+
+    private val okClient = OkHttpClient.Builder()
+        .addNetworkInterceptor(StethoInterceptor())
+        .build()
+
+    override fun onCreate() {
+        super.onCreate()
+        Stetho.initializeWithDefaults(this)
+    }
+
     // репозиторий
     private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
@@ -33,6 +46,7 @@ class App:Application() {
 
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okClient)
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
